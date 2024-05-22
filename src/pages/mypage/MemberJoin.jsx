a39c3f71e66c73064c053152250caa076ea78386
billihazero <billihazero@gmail.com>
2024-05-22 18:52:29 +0900

    24.05.22 회원가입 페이지 수정

--- src/pages/mypage/MemberJoin.jsx
+++ src/pages/mypage/MemberJoin.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 import { useNavigate } from 'react-router-dom';
 import "./MemberJoin.css";
-
+import Button from "../../component/common/Button";
 
 function MemberJoin(props) {
 const navigate = useNavigate();
@@ -18,39 +18,90 @@ const navigate = useNavigate();
   const [emailDomain, setEmailDomain] = useState("gmail.com"); // 기본값 설정
   const [emailDomainInput, setEmailDomainInput] = useState(false);
   const [isIdAvailable, setIsIdAvailable] = useState(null);
-  const [isEmailAvailable, setIsEmailAvailable] = useState(null);
   const [isNickNameAvailable, setIsNickNameAvailable] = useState(null);
-  const [isPhoneNumAvailable, setIsPhoneNumAvailable] = useState(null);
   const [profileImage, setProfileImage] = useState(null);
   const [birth, setBirth] = useState("");
-  const [gender, setGender] = useState("");
+  const [gender, setGender] = useState("남성");
 
-  const emailDomains = ["gmail.com", "naver.com", "hotmail.com", "yahoo.com", "직접입력"];
+  //ID 
+  const handleIdChange = event => {
+    setUserId(event.target.value);
+    setIsIdAvailable(null);
+  };
 
+  //ID 유효성 검사 
+  const handleCheckIdAvailability = async () => {
+    try {
+      const response = await axios.post('/checkIdAvailability/idCheck', userId); 
+      
+
+      console.log('응답 값:', response.data);
+      setIsIdAvailable(response.data);
+
+    }catch (error) {
+      console.error('Error occurred while checking ID availability:', error);
+    }
+  };
+
+  //이름
+  const handlenNameChange = event => {
+    setUsername(event.target.value);
+  };
+
+  //nickname
+  const handlenickNameChange = event => {
+    setNickName(event.target.value);
+  };
+
+  //nickname 유효성 검사.
+  const handleCheckNickNameAvailability = async () => {
+    try {
+      const response = await axios.post('/checkIdAvailability/nickNameCheck', nickName); 
+
+    console.log('응답 값:', response.data);
+    setIsNickNameAvailable(response.data);
+  
+    } catch (error) {
+      console.error('Error occurred while checking ID availability:', error);
+    }
+  };
+
+  //password
+  const handlePasswordChange = event => {
+    setPassword(event.target.value);
+  };
+
+  //프로필 이미지
   const handleProfileImageChange = event => {
     const selectedFile = event.target.files[0];
     setProfileImage(selectedFile);
   };
 
-  const handlePhoneNumberChange = (event, setter) => {
+  //핸드폰 번호 값
+  const handlePhoneNumberChange = event => {
     let value = event.target.value.replace(/\D/g, ''); // 숫자 이외의 문자 제거
     // 최대 3자리까지만 허용  
-    value = value.substring(0, 3);
-    setter(value);
+    setPhoneNumber1(value);
   };
-  
 
-  const handlePhoneNumberChange2 = (event, setter) => {
+  const handlePhoneNumberChange2 = event => {
     let value = event.target.value.replace(/\D/g, '');
-    setter(value);
+    setPhoneNumber2(value);
     
   };
 
-  const handlePhoneNumberChange3 = (event, setter) => {
+  const handlePhoneNumberChange3 = event => {
     let value = event.target.value.replace(/\D/g, '');
-    setter(value);
+    setPhoneNumber3(value);
     
   };
+
+  //이메일
+  const handleEmailChange = event =>{
+    setEmailNum1(event.target.value);
+  }
+
+  const emailDomains = ["gmail.com", "naver.com", "hanmail.com", "yahoo.com", "직접입력"];
 
   const handleEmailDomainChange = event => {
     const value = event.target.value;
@@ -63,100 +114,17 @@ const navigate = useNavigate();
     }
 };
 
-  const handleIdChange = event => {
-    setUserId(event.target.value);
-    setIsIdAvailable(null);
-  };
-// ID 유효성 검사 
-  const handleCheckIdAvailability = async () => {
-    try {
-
-      const response = await axios.post('/checkIdAvailability/idCheck', userId); 
-
-    console.log('응답 값:', response.data);
-    setIsIdAvailable(response.data);
-
-      console.log('응답 값:', response.data);
-      setIsIdAvailable(response.data);
-  
-      // 중복 확인 결과에 따라 메시지를 표시합니다.
-      if (response.data === 0) {
-        alert("사용 가능한 아이디입니다.");
-      } else if (response.data === 1)  {
-        alert("이미 사용 중인 아이디입니다.");
-      }
-    } catch (error) {
-      console.error('Error occurred while checking ID availability:', error);
-    }
-  };
-// email 유효성 검사
-  const handleCheckEmailAvailability = async () => {
-    try {
-
-      const response = await axios.post('/checkIdAvailability/emailCheck', emailNum1); 
-
-    console.log('응답 값:', response.data);
-    setIsIdAvailable(response.data);
-
-      console.log('응답 값:', response.data);
-      setIsIdAvailable(response.data);
-  
-      // 중복 확인 결과에 따라 메시지를 표시합니다.
-      if (response.data === 0) {
-        alert("사용 가능한 이메일입니다.");
-      } else if (response.data >= 1)  {
-        alert("이미 사용 중인 이메일입니다.");
-      }
-    } catch (error) {
-      console.error('Error occurred while checking ID availability:', error);
-    }
-  };
-// nickname 유효성 검사.
-  const handleCheckNickNameAvailability = async () => {
-    try {
-      const response = await axios.post('/checkIdAvailability/nickNameCheck', nickName); 
-
-    console.log('응답 값:', response.data);
-    setIsIdAvailable(response.data);
-
-      console.log('응답 값:', response.data);
-      setIsIdAvailable(response.data);
-  
-      // 중복 확인 결과에 따라 메시지를 표시합니다.
-      if (response.data === 0) {
-        alert("사용 가능한 닉네임입니다.");
-      } else if (response.data >= 1)  {
-        alert("이미 사용 중인 닉네임입니다.");
-      }
-    } catch (error) {
-      console.error('Error occurred while checking ID availability:', error);
-    }
-  };
-// phoneNum 유효성 검사
-
-  const handleCheckPhoneNumAvailability = async () => {
-    try {
-
-      const response = await axios.post('/checkIdAvailability/phoneNumCheck', [phoneNum1, phoneNum2, phoneNum3]
-
-      );
-    console.log('응답 값:', response.data);
-    setIsIdAvailable(response.data);
-
-      console.log('응답 값:', response.data);
-      setIsIdAvailable(response.data);
-  
-      // 중복 확인 결과에 따라 메시지를 표시합니다.
-      if (response.data === 0) {
-        alert("사용 가능한 번호입니다.");
-      } else if (response.data >= 1)  {
-        alert("이미 사용 중인 번호입니다.");
-      }
-    } catch (error) {
-      console.error('Error occurred while checking ID availability:', error);
-    }
+  //생년월일
+  const handleBirthChange = event => {
+    setBirth(event.target.value);
   };
 
+  //성별
+  const handleGenderChange = event => {
+    setGender(event.target.value);
+  }
+
+  //회원 정보 DB로 전달
   const handleSubmit = async () => {
     const userInfo = {
       userId,
@@ -170,17 +138,19 @@ const navigate = useNavigate();
       emailNum1,
       emailNum2: emailDomain,
       birth,
-      gender: gender === "남성" ? "F" : "M",
+      gender: gender === "male" ? "M" : "F",
     };
 
     try {
       const response1 = await axios.post('/addmember', userInfo, {
-
+        
         headers: {
           'Content-Type': 'application/json'
         }
       });
-  
+
+      alert("회원가입이 완료되었습니다.");
+
       // 이미지가 있는 경우에만 이미지를 업로드하는 axios 요청을 보냅니다.
       if (profileImage) {
         const formData = new FormData();
@@ -188,22 +158,20 @@ const navigate = useNavigate();
 
         const response2 = await axios.post('/addmember/image', formData);
 
-        
         if (response1.status === 200 && response2.status === 200) {
           const data1 = response1.data;
           const data2 = response2.data;
-          console.log(data1, data2);
+          
         } else {
-          console.error('회원가입(중복검사 해주세요) 또는 이미지 업로드에 실패했습니다.');
           alert('회원가입(중복검사 해주세요) 또는 이미지 업로드에 실패했습니다.');
         }
-      } else {
+
+      }else {
+
         if (response1.status === 200) {
           const data1 = response1.data;
-          console.log(data1);
           navigate('/');
         } else {
-          console.error('회원가입(중복검사 해주세요)에 실패했습니다.');
           alert('회원가입(중복검사 해주세요)에 실패했습니다.');
         }
       }
@@ -212,103 +180,120 @@ const navigate = useNavigate();
     }
   };
 
-
   return (
-    <>
-      <h2 className="join-title">회원가입</h2>
-      <div className='totalcontainer'>
-        <p>아이디:</p>
-        <p>
-          <input className="login" type="text" placeholder="아이디" value={userId} onChange={handleIdChange} />
-        </p>
-        <button className='btn1' onClick={handleCheckIdAvailability}>중복 확인</button>
-        {isIdAvailable === true ? <span style={{ color: 'green' }}>사용 가능한 아이디입니다.</span> : isIdAvailable === false ? <span style={{ color: 'red' }}>이미 사용 중인 아이디입니다.</span> : null}
-        
-        <p>사용자 이름:</p>
-        <p>   
-          <input className="login" type="text" placeholder="사용자 이름" value={username} onChange={event => setUsername(event.target.value)} />
-        </p>
-        
-        <p>닉네임:</p>
-        <p>
-          <input className="login" type="text" placeholder="닉네임" value={nickName} onChange={event => setNickName(event.target.value)}  />
-        </p>
-        <button className='btn1' onClick={handleCheckNickNameAvailability}>중복 확인</button>
-        {isNickNameAvailable === true ? <span style={{ color: 'green' }}>사용 가능한 닉네임입니다.</span> : isNickNameAvailable === false ? <span style={{ color: 'red' }}>이미 사용 중인 닉네임입니다.</span> : null}
-        
-        <p>비밀번호:</p>
-        <p>
-          <input className="login" type="password" placeholder="비밀번호" onChange={event => setPassword(event.target.value)} />
-        </p>
-        
-        <p>프로필 이미지:</p>
-        <p>
-          <input className="login4" type="file" accept="image/*" onChange={handleProfileImageChange} />
-        </p>
-        
-        <p>핸드폰 번호:</p>
-        <p className='phone-group'>
-          <input className="login1" type="text" maxLength="3" value={phoneNum1} onChange={event => handlePhoneNumberChange(event, setPhoneNumber1)} /> -
-          <input className="login2" type="text" maxLength="4" value={phoneNum2} onChange={event => handlePhoneNumberChange2(event, setPhoneNumber2)} /> -
-          <input className="login3" type="text" maxLength="4" value={phoneNum3} onChange={event => handlePhoneNumberChange3(event, setPhoneNumber3)} />
-        </p>
-        <button className='btn1' onClick={handleCheckPhoneNumAvailability}>중복 확인</button>
-        {isPhoneNumAvailable === true ? <span style={{ color: 'green' }}>사용 가능한 핸드폰 번호입니다.</span> : isPhoneNumAvailable === false ? <span style={{ color: 'red' }}>이미 사용 중인 핸드폰 번호입니다.</span> : null}
-        
-        <p>이메일:</p>
-        <p className='email-group'>
-          <input className="login5" type="text" placeholder="아이디" onChange={event => setEmailNum1(event.target.value)} />
-          @
-          {!emailDomainInput && (
-            <select value={emailDomain} onChange={handleEmailDomainChange}>
-              {emailDomains.map(domain => (
-                <option key={domain} value={domain}>{domain}</option>
-              ))}
-            </select>
-          )}
-          {emailDomainInput &&
-            <input className="login6" type="text" placeholder="도메인을 직접 입력하세요" onChange={event => setEmailDomain(event.target.value)} />
-          }
-        </p>
-        <button className='btn1' onClick={handleCheckEmailAvailability}>중복 확인</button>
-        {isEmailAvailable === true ? <span style={{ color: 'green' }}>사용 가능한 이메일입니다.</span> : isEmailAvailable === false ? <span style={{ color: 'red' }}>이미 사용 중인 이메일입니다.</span> : null}
-        
-        <p>생년월일:</p>
-        <p>
-        <input type="date" name="userBirth" value={birth.setBirth} onChange={handleSubmit} placeholder="생년월일" className='login'/>
-        </p>
-        
-        <p>성별:</p>
-        <p>
-          <select className="gender-select" onChange={event => setGender(event.target.value)}>
-            <option value="남성">남성</option>
-            <option value="여성">여성</option>
-          </select>
-        </p>
-        <p><input className="btn" type="submit" value="회원가입" onClick={handleSubmit} /></p>
-      </div>
-  
-      <p className="login-return">로그인화면으로 돌아가기  <button  className='btn1' onClick={() => {
-        props.setMode("LOGIN");
-      }}>로그인</button></p>
+    
+    <div className='totalContainer'>
 
-    </>
+      <div className='mainBanner'>
+        <img src='/images-jsx/밤양갱_black.svg' />
+      </div>
+
+      <div className='joinTitle'><h2>회원가입</h2></div>
+
+      <div className='container'>
+        <div className='topArea'>
+          <div className='idArea'>
+            <div className='idBox'>
+              <input type="text" placeholder='아이디' value={userId} onChange={handleIdChange} className='inputBox'/>
+              <div className='btnArea'>
+               <Button text={"중복확인"} type={"idCheckBtn"} onClick={handleCheckIdAvailability} />
+              </div>
+            </div>
+
+            {isIdAvailable === 0 ? 
+            <div className='idCheck' style={{ color: 'black' }}> 사용 가능한 아이디입니다.</div> : 
+            isIdAvailable === 1 ? 
+            <div className='idCheck' style={{ color: 'red' }}> 이미 사용 중인 아이디입니다.</div> : 
+            null}
+          </div>
+
+          <div className='passwdArea'>
+            <div>
+              <input type='password' placeholder='비밀번호' onChange={handlePasswordChange} value={password} className='inputBox'/>
+            </div>
+          </div>
+
+          <div className='nickNameArea'>
+            <div className='nickNameBox'>
+                <input type='text' placeholder='닉네임' value={nickName} onChange={handlenickNameChange} className='inputBox'/>
+                <div className='btnArea'>
+                  <Button text={"중복확인"} type={"nickNameCheckBtn"} onClick={handleCheckNickNameAvailability}/>
+                </div>
+            </div>
+
+            {isNickNameAvailable === 0 ? 
+            <div className='idCheck' style={{ color: 'black' }}> 사용 가능한 닉네임입니다.</div> : 
+            isNickNameAvailable >= 1 ? 
+            <div className='idCheck' style={{ color: 'red' }}> 이미 사용 중인 닉네임입니다.</div> : 
+            null}
+
+          </div>
+        </div>
+
+        <div className='middleArea'>
+          <div className='nameArea'>
+            <div>
+              <input type="text" placeholder='이름' value={username} className='inputBox' onChange={handlenNameChange}/>
+            </div>
+          </div>
+
+          <div className='birthArea'>
+              <input type='text' placeholder='생년월일 6자리를 입력해주세요' value={birth} onChange={handleBirthChange} className='inputBox'/>
+          </div>
+
+          <div className='phoneArea'>
+              <div>
+                <input type="text" placeholder="01X" maxLength={3} value={phoneNum1} onChange={handlePhoneNumberChange} className='phoneFirstInputBox'/> -
+                <input type="text" placeholder="●●●●" maxLength={4} value={phoneNum2} onChange={handlePhoneNumberChange2} className='phoneInputBox'/> -
+                <input type="text" placeholder="●●●●" maxLength={4} value={phoneNum3} onChange={handlePhoneNumberChange3} className='phoneInputBox'/>
+              </div>
+          </div>
+          
+          <div className='emailArea'>
+              <input type="text" value={emailNum1} placeholder='이메일' onChange={handleEmailChange} className='emailInputBox'/> @ 
+                {emailDomainInput ? (
+                      <input type="text" placeholder='도메인을 직접 입력하세요' onChange={handleEmailDomainChange} className='domainBox'/>
+                  ) :(
+                      <select value={emailDomain} onChange={handleEmailDomainChange} className='domainSelect'>{emailDomains.map(domain => (
+                        <option key={domain} value={domain}>{domain}</option>))}
+                      </select>
+                    )
+                }
+          </div>
+        </div>
+
+        <div className='genderArea'>
+          <div className='maleBox'>
+            <label className={`genderButton ${gender === 'male' ? 'active' : ''}`} htmlFor='male'>
+              <input type='radio' id='male' value='male' className='genderType' checked={gender === 'male'} onChange={handleGenderChange} />
+              남성
+            </label>
+          </div>
+          <div className='femaleBox'>
+            <label className={`genderButton ${gender === 'female' ? 'active' : ''}`} htmlFor='female'>
+              <input type='radio' id='female' value='female' className='genderType' checked={gender === 'female'} onChange={handleGenderChange} />
+              여성
+            </label>
+         </div>
+        </div>
+
+        <div className='profileArea'>
+          <div className='profileTitle'>프로필 이미지 설정</div>
+            <div>
+              <input className="profileInputBox" type="file" accept="image/*" onChange={handleProfileImageChange} />
+            </div>
+        </div>
+      </div>
+
+        <div className='submitArea'>
+          <div className='submitBtnArea'>
+            <Button text={"회원가입"} type={"submitBtn"} onClick={handleSubmit} />
+          </div>
+        </div>
+    </div>
   );
   
 }
-
-function App() {
-  const [mode, setMode] = useState("SIGNIN");
-
-  return (
-    <>
-      <div className="background">
-        <Signin setMode={setMode}></Signin>
-      </div>
-    </>
-  );
-}
-
 
 
 export default MemberJoin;
